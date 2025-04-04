8fbdaea4dbf50f2b99a1ed36140194a65ba7d426
승혁이 <94110438+3759357@users.noreply.github.com>
2025-04-04 09:30:41 +0900

    Add files via upload
    초기 세팅

---
+++ kyobodts/src/main/java/com/kyobodts/KyobodtsApplication.java
@@ -0,0 +1,13 @@
+package com.kyobodts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyobodtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyobodtsApplication.class, args);
+	}
+
+}
